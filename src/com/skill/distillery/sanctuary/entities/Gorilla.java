361c4ee1257eb3a70c74c867ed82ba20c4e1d795
William Brown <wsbrown.colo@gmail.com>
2024-11-01 17:32:14 -0600

    subclass gorilla complete

--- src/com/skill/distillery/sanctuary/entities/Gorilla.java
+++ src/com/skill/distillery/sanctuary/entities/Gorilla.java
@@ -1,5 +1,18 @@
 package com.skill.distillery.sanctuary.entities;
 
-public class Gorilla extends Animal{
-
+public class Gorilla extends Animal {
+	private static String noise = "ROOAAR";
+	
+	@Override
+	public void makeNoise() {
+		System.out.println(noise);
+	}
+	
+	
+	
+	@Override
+	public void eat(int amount) {
+		makeNoise();
+		System.out.println("WHERE'S THE BEEF?" + " Gorilla eats " + amount + " heads of lettuce");
+	}
 }
